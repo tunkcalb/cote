642ea97b1262821692d76fcbd442c3d8860793bc
tunkcalb <95354899+tunkcalb@users.noreply.github.com>
2022-06-26 15:03:00 +0900

    Draft (#2)
    * study
    
    * study
    
    * study

---
+++ baek/2667.py
@@ -0,0 +1,54 @@
+def find_house(n, board):
+    house = []
+    for col in range(n):
+        for row in range(n):
+            if board[(row,col)] >0 :
+                house.append((row,col))
+    return house
+
+def generate_candidates(n, board):
+    house = find_house(n, board)
+    village = []
+    while len(house) > 0:
+        candidates = [house.pop(0)]
+
+        while True:
+            connected = []
+            for h in house:
+                if (h[0], h[1] - 1) in candidates:
+                    connected.append(h)
+                elif (h[0], h[1] + 1) in candidates:
+                    connected.append(h)
+                elif (h[0] - 1, h[1]) in candidates:
+                    connected.append(h)
+                elif (h[0] + 1, h[1]) in candidates:
+                    connected.append(h)
+
+            if len(connected) == 0:
+                break
+            
+            for c in connected:
+                house.remove(c)
+            candidates.extend(connected)
+
+        village.append(candidates)
+    
+    def key(elem):
+        return len(elem)
+
+    village.sort(key=key)
+    return village
+
+
+n = int(input())
+board = {}
+
+for col in range(n):
+    line = input()
+    for row in range(n):
+        pos = (row, col)
+        board[pos] = int(line[row])
+village = generate_candidates(n, board)  # village = [[(int, int), ...], ...]
+print(len(village))
+for v in village:
+    print(len(v))
--- baek/7568.py
+++ baek/7568.py
@@ -4,12 +4,10 @@ build_list = []
 for i in range(n):
     x,y = [int(num) for num in input().split()]
     build_list.append((x,y))
-build_dic = {build : 1 for build in build_list}
 
 for build in build_list:
+    count = 1
     for rank in build_list:
         if build[0] < rank[0] and build[1] < rank[1]:
-            build_dic[build] += 1
-
-for build in build_list:
-    print(build_dic[build], end = " ")
+            count += 1
+    print(count, end = " ")
