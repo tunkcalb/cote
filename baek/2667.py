59ccf84c7529a346ba92b44c5e9bf6171c193e7d
tunkcalb <wjdgusdndi@gmail.com>
2022-06-26 15:01:06 +0900

    study

--- baek/2667.py
+++ baek/2667.py
@@ -9,36 +9,34 @@ def find_house(n, board):
 def generate_candidates(n, board):
     house = find_house(n, board)
     village = []
-    for i in range(len(house)):
-        if i > len(house):
-            return
-        candidates = []
-        house_idx = 0
-        candidates.append(house[0])
-        house.pop(0)
-        
+    while len(house) > 0:
+        candidates = [house.pop(0)]
+
         while True:
-            if (house[house_idx][0], house[house_idx][1]-1) in candidates:
-                candidates.append(house[house_idx])
-                house.pop[house_idx]
-                return True
-            elif (house[house_idx][0], house[house_idx][1]+1) in candidates:
-                candidates.append(house[house_idx])
-                house.pop[house_idx]
-                return True
-            elif (house[house_idx][0]-1, house[house_idx][1]) in candidates:
-                candidates.append(house[house_idx])
-                house.pop[house_idx]
-                return True
-            elif (house[house_idx][0]+1, house[house_idx][1]) in candidates:
-                candidates.append(house[house_idx])
-                house.pop[house_idx]
-                return True
-            else:
-                house_idx += 1
-            if house_idx > len(house):
-                return False
+            connected = []
+            for h in house:
+                if (h[0], h[1] - 1) in candidates:
+                    connected.append(h)
+                elif (h[0], h[1] + 1) in candidates:
+                    connected.append(h)
+                elif (h[0] - 1, h[1]) in candidates:
+                    connected.append(h)
+                elif (h[0] + 1, h[1]) in candidates:
+                    connected.append(h)
+
+            if len(connected) == 0:
+                break
+            
+            for c in connected:
+                house.remove(c)
+            candidates.extend(connected)
+
         village.append(candidates)
+    
+    def key(elem):
+        return len(elem)
+
+    village.sort(key=key)
     return village
 
 
@@ -50,7 +48,7 @@ for col in range(n):
     for row in range(n):
         pos = (row, col)
         board[pos] = int(line[row])
-village = generate_candidates(n, board)
+village = generate_candidates(n, board)  # village = [[(int, int), ...], ...]
 print(len(village))
-for i in range(village):
-    print(len(village[i]))
+for v in village:
+    print(len(v))
