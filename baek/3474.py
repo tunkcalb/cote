cf0478ddbbc0fc40969aaa756835644576ec7f5d
tunkcalb <wjdgusdndi@gmail.com>
2022-06-28 03:30:24 +0900

    study

---
+++ baek/3474.py
@@ -0,0 +1,9 @@
+import sys
+T = int(sys.stdin.readline())
+for i in range(T):
+    N = int(sys.stdin.readline())
+    result = 0
+    while N >= 1 :
+        result += N//5
+        N /= 5
+    print(int(result))
