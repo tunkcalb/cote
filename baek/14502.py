a5f88258b5f6dcd62f06ae8904600f357a85ce78
tunkcalb <wjdgusdndi@gmail.com>
2022-07-09 01:09:13 +0900

    study/3

---
+++ baek/14502.py
@@ -0,0 +1,52 @@
+import sys
+import copy
+from itertools import combinations
+
+def wall_construct(empty, wall_num, board):
+    for wall_comb in combinations(empty,wall_num):
+        board_wall = copy.deepcopy(board)
+        for x_w, y_w in wall_comb:
+            board_wall[x_w, y_w] = 1
+        virus(board_wall)
+
+def virus(board_wall):
+    
+    #바이러스 위치
+    virus = [(n,m) for n in range(N) for m in range(N) if board_wall[(n,m)] == 2]
+
+    while virus:
+        x, y = virus.pop()
+        for dx, dy in direction:
+            nx = x + dx
+            ny = y + dy
+            if 0 <= nx < N and 0 <= ny < M and board_wall[(nx, ny)] == 0:
+                board_wall[(nx, ny)] = 2
+                virus.append((nx, ny))
+    safezone(board_wall)
+
+def safezone(board_wall):
+    global answer
+    safezone_count = 0
+
+    for key in board_wall:
+        if board_wall[key] == 0:
+            safezone_count += 1
+        answer = max(answer , safezone_count)
+
+board = {}
+N, M = map(int, sys.stdin.readline().rstrip().split())
+
+for row in range(N):
+    line = sys.stdin.readline().rstrip().split()
+    for col in range(M):
+        pos = (row,col)
+        board[pos] = int(line[col])
+
+empty = [(n, m) for n in range(N) for m in range(M) if board[(n, m)] == 0]
+direction = [(-1,0) , (1, 0), (0, -1), (0, 1)]
+answer = 0
+wall_num = 3
+
+
+wall_construct(empty, wall_num, board)
+print(answer)
