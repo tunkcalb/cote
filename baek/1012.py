5a5525c763fc57fdde480afcbe20a085abec7e19
tunkcalb <95354899+tunkcalb@users.noreply.github.com>
2022-07-16 14:42:54 +0900

    study/4 (#5)
    * study/4
    
    * editorconfig
    
    * study/4
    
    * style: reformat 1012.py with black

---
+++ baek/1012.py
@@ -0,0 +1,47 @@
+import sys
+
+num = int(sys.stdin.readline().rstrip())
+answer = []
+for _ in range(num):
+    c_list = []
+    cabbage = []
+    M, N, K = map(int, sys.stdin.readline().rstrip().split())
+    for _ in range(K):
+        X, Y = map(int, sys.stdin.readline().rstrip().split())
+        if X <= M - 1 and Y <= N - 1:
+            cabbage.append((X, Y))
+    while cabbage:
+        connected = [cabbage.pop(0)]
+        fresh_cabbage = []
+        while True:
+            for c in cabbage:
+                if (c[0] - 1, c[1]) in connected:
+                    fresh_cabbage.append(c)
+                elif (c[0] + 1, c[1]) in connected:
+                    fresh_cabbage.append(c)
+                elif (c[0], c[1] - 1) in connected:
+                    fresh_cabbage.append(c)
+                elif (c[0], c[1] + 1) in connected:
+                    fresh_cabbage.append(c)
+            if len(fresh_cabbage) == 0:
+                break
+            for f in fresh_cabbage:
+                cabbage.remove(f)
+            connected.extend(fresh_cabbage)
+            fresh_cabbage.clear()
+
+        c_list.append(connected)
+    answer.append(c_list)
+for i in range(len(answer)):
+    print(len(answer[i]))
+
+"""
+[(1, 2), (2, 2)], [(3, 3), (3, 4)]
+(2, 3)
+[(1, 2), (2, 2), (2, 3)], [(3, 3), (3, 4)]
+
+1 1
+4 1
+2 1
+3 1
+"""
